8d00b1ca3805e36b0df2d30b62f857d321711bb2
jymyte <jyrimy@gmail.com>
2023-01-13 17:26:47 +0200

    Remove unintended submodule

---
+++ firebase-todolist/App.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Button, TextInput, Alert, ScrollView } from 'react-native'
+import { child, push, ref, remove, update, onValue } from 'firebase/database'
+import { db, TODOS_REF } from './firebase/Config' ;
+import TodoItem from './components/TodoItem';
+
+export default function App() {
+  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState({});
+
+  useEffect(() => {
+    const todoItemsRef = ref(db, TODOS_REF);
+    onValue(todoItemsRef, (snapshot) => {
+      const data = snapshot.val() ? snapshot.val() : {};
+      const todoItems = {...data};
+      setTodos(todoItems);
+    })
+  }, [])
+  
+
+  const addNewTodo = () =>{
+    console.log(newTodo, "newtodo");
+    if (newTodo.trim !== "") {
+      const newTodoItem = {
+        done: false,
+        todoItem: newTodo
+      }
+      const newTodoItemKey = push(child(ref(db), TODOS_REF)).key;
+      const updates = {};
+      updates[TODOS_REF + newTodoItemKey] = newTodoItem;
+      setNewTodo('');
+      return update(ref(db), updates);
+    }
+  }
+
+  const removeTodos = () => {
+    remove(ref(db), TODOS_REF);
+  }
+
+  const createTwoButtonAlert = () => {
+    Alert.alert("Todolist", "Remove all items?", [{
+      text: "Cancel",
+      onPress: () => console.log("Cancel pressed"),
+      style: "cancel"
+      },
+      {
+        text: "OK", onPress: () => removeTodos()
+      }],
+      { cancelable: false }
+    );
+  }
+
+  let todosKeys = Object.keys(todos);
+
+  return (
+    <View 
+      style={styles.container}
+      contentContainerStyle={styles.contentContainerStyle} >
+      <Text style={styles.header}>Todolist ({todosKeys.length})</Text>
+      <View style={styles.newltem} >
+        <TextInput 
+          placeholder='Add new todo'
+          value={newTodo}
+          style={styles.textInput}
+          onChangeText={setNewTodo}
+      />
+      </View>
+      <View style={styles.buttonStyle}>
+        <Button 
+          title="Add new Todo item"
+          onPress={() => addNewTodo()}
+        />
+      </View>
+      <ScrollView>
+        {todosKeys.length > 0 ? (
+          todosKeys.map(key => (
+            <TodoItem
+              key={key}
+              id={key}
+              todoItem={todos[key]}
+            />
+          ))
+        ) : (
+          <Text style={styles.infoText}>There are no items</Text>
+        )}
+        <View style={styles.buttonStyle}>
+          <Button
+            title="Remove all todos"
+            onPress={() => createTwoButtonAlert()} />
+        </View>
+      </ScrollView>
+      <View style={styles.buttonStyle}>
+      <Button 
+          title="Remove all todos"
+          onPress={() => createTwoButtonAlert()}
+        />
+      </View>
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    marginTop: 50,
+    marginLeft: 30,
+    height: "20%",
+  },
+  contentContainerStyle: { alignitems: "flex-start" },
+  header: { fontSize: 30 },
+  newltem: { marginVertical: 10, alignitems: "flex-start" },
+  infoText: { margintop: 5, marginBottom: 5, fontSize: 15 },
+  buttonStyle: { margintop: 10, marginBottom: 10, width: "80%" },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#afafaf",
+    width: "80%",
+    borderRadius: 5,
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    marginVertical: 20,
+    fontSize: 18,
+  },
+});
